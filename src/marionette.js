414cc6dad12239e4ae76741a811e005107964b91
Michel Lampo <michel@creatingfuture.eu>
2022-01-07 20:44:51 +0100

    Convert JQuery to Vanilla JS Add example

---
+++ src/marionette.js
@@ -0,0 +1,80 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const live = function (eventType, selector, handler) {
+        document.addEventListener(eventType, e => {
+            if (e.target.closest(selector)) {
+                handler(e);
+            }
+        });
+    }
+
+    if (typeof (history.pushState) !== 'undefined') {
+
+        // history is supported; do magical things
+
+        // hijack the nav click event
+        live('click', 'a.internal', function (e) {
+            const _href = e.target.getAttribute("href");
+
+            // change the url without a page refresh and add a history entry.
+            history.pushState(null, null, _href);
+
+            // load the content
+            loadContent(_href); // fear not! we're going to build this function in the next code block
+
+            e.preventDefault();
+        });
+
+        live('submit', 'form.internal', function (e) {
+            const url = e.target.getAttribute('action') || window.location.href;
+
+            loadFormContent(url, form.serialize());
+
+            e.preventDefault();
+        });
+    }
+
+    window.addEventListener('popstate', (event) => {
+        const link = location.pathname.replace(/^.*[\/]/, ""); // get filename only
+        loadContent(link);
+    });
+
+
+    // set up some variables
+    const $mainContent = document.querySelector("#content-wrapper");
+
+    function loadContent(href) {
+        // TODO fade out
+
+        fetch(href).then(response => {
+            return response.text();
+        }).then(data => {
+            const domData = new DOMParser().parseFromString(data, "text/html");
+            const content = domData.querySelector('#content');
+            $mainContent.replaceChildren(content);
+            // TODO fade in
+
+        }).catch(error => {
+            console.log(error)
+        });
+    }
+
+    function loadFormContent(href, data) {
+        console.log("LoadFormcontent");
+        // TODO fade out
+
+        fetch(href, {
+            method: 'POST',
+            body: data
+        }).then(response => {
+            return response.text();
+        }).then(data => {
+            const domData = new DOMParser().parseFromString(data.body, "text/html");
+            const content = domData.querySelector('#content');
+            $mainContent.replaceChildren(content);
+            // TODO fade in
+        }).catch(error => {
+            console.log(error)
+        });
+    }
+
+});
